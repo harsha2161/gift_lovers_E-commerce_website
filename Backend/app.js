3a5b4e51728e9017e3a58bff874e433fd4c57c3d
R.M.P Harsha <prabhathharsha77@gmail.com>
2026-05-24 02:08:50 +0530

    build: add dependencis

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,16 @@
 import express from 'express';
-
+import apiRouter from './api/index.js';
+import { errorHandler } from './api/middlewares/errorHandler.js';
+import bodyParser from 'body-parser';
+import cors from "cors";
 const app = express()
 
 app.use(express.json())
+app.use(cors()) 
+app.use(bodyParser.json())
 
+app.use('/api/v1', apiRouter)
+
+app.use(errorHandler)
 
 export default app
