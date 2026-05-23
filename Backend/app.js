868875c8833aaed89d7611ad1dc18075a221f129
R.M.P Harsha <prabhathharsha77@gmail.com>
2026-05-24 03:24:14 +0530

    fix: remove bodyparser

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import apiRouter from './api/index.js';
 import { errorHandler } from './api/middlewares/errorHandler.js';
-import bodyParser from 'body-parser';
 import cors from "cors";
 const app = express()
 
 app.use(express.json())
 app.use(cors()) 
-app.use(bodyParser.json())
 
 app.use('/api/v1', apiRouter)
 
