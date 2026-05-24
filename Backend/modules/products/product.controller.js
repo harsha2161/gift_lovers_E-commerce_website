bc169c6ca6dfe2b979bb73bf67550e57dc8ebb6c
R.M.P Harsha <prabhathharsha77@gmail.com>
2026-05-24 11:38:01 +0530

    feat: create function for viewproducts,deleteproducts,updateproducts,viewproducts

--- Backend/modules/products/product.controller.js
+++ Backend/modules/products/product.controller.js
@@ -1,10 +1,50 @@
-import AddProduct from "./product.service.js";
+import {AddProduct , DeleteProduct, UpdateProduct, ViewProduct, ViewProductById } from "./product.service.js";
 
 export async function addProduct(req, res, next){
 
     try{
         const products = await AddProduct(req.body)
         res.status(201).json({ message : "Product added successfully", data : products})
+    }catch(error){
+        next(error)
+    }
+}
+
+export async function viewProduct(req, res, next){
+    
+    try{
+        const products = await ViewProduct(req.user)
+        res.status(201).json({data: products})
+    }catch(error){
+        next(error)
+    }
+}
+
+export async function deleteProduct(req, res, next){
+
+    try{
+        await DeleteProduct(req.params.productId)
+        res.status(201).json({message : `product ${req.params.productId} is deleted successfully`})
+    }catch(error){
+        next(error)
+    }
+}
+
+export async function updateProduct(req,res, next){
+    
+    try{
+        await UpdateProduct(req.body, req.params.productId)
+        res.status(201).json({message : `product ${req.params.productId} updated`}) 
+    }catch(error){
+        next(error)
+    }
+}
+
+export async function viewProductById(req,res,next){
+    
+    try{
+        const product = await ViewProductById(req.params.productId)
+        res.status(201).json({data : product})
     }catch(error){
         next(error)
     }
--- Backend/modules/products/product.service.js
+++ Backend/modules/products/product.service.js
@@ -1,10 +1,33 @@
 import product from "./product.model.js";
 
-export default async function AddProduct(productsDetials){
+export async function AddProduct(productsDetials){
     
     const NewProduct = new product({
         ...productsDetials
     })
 
     return await NewProduct.save()
+}
+
+export async function ViewProduct(user){
+   
+    if(user.role == "admin"){
+        return await product.find()
+
+    }else if(user.role == "user"){
+        return await product.find({isAvailable : true})
+    }
+    
+}
+
+export async function DeleteProduct(productId){
+    return await product.deleteOne({productId : productId})
+}
+
+export async function UpdateProduct(updateDetails, productId){
+    return await product.updateOne({productId : productId}, updateDetails)
+}
+
+export async function ViewProductById(productId){
+    return await product.findOne({productId : productId})
 }
