58728895da35e6a5f11aaa63ad53f7ac631ffa9f
R.M.P Harsha <prabhathharsha77@gmail.com>
2026-05-27 00:14:54 +0530

    Created a reusable PageLayout component using Outlet to persist the Header and Footer across different routes.

---
+++ Frontend/src/components/layout.jsx
@@ -0,0 +1,13 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+
+export default function PageLayout(){
+ return (
+    <div className="min-h-screen bg-bgcolor1">
+      <Header />
+      <Footer />
+    </div>
+  )
+
+}
