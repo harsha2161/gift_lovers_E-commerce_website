1ee8a11de773dbf4380004424d971c9444528558
R.M.P Harsha <prabhathharsha77@gmail.com>
2026-05-26 19:53:56 +0530

    fix: order.service fixed

--- Backend/modules/orders/order.service.js
+++ Backend/modules/orders/order.service.js
@@ -4,15 +4,15 @@ import AppError from "../../shared/errors/AppError.js";
 
 
 export async function CreateOrder(orderInfo, orderUser) {
-   
+
     const customerName = orderUser.firstName + " " + orderUser.lastName;
 
     let newOrderId = "ORD00001";
-    const lastOrder = await Order.find().sort({date : -1}).limit(1);
+    const lastOrder = await Order.find().sort({ date: -1 }).limit(1);
 
-    if(lastOrder.length > 0){
+    if (lastOrder.length > 0) {
         newOrderId = lastOrder[0]?.orderId ? "ORD" + String(parseInt(lastOrder[0].orderId.replace("ORD", "")) + 1).padStart(5, '0') : "ORD00001";
-     
+
     }
 
     let total = 0;
@@ -27,29 +27,28 @@ export async function CreateOrder(orderInfo, orderUser) {
         const item = await Product.findOne({ productId: currentProduct.productId });
 
         if (!item) {
-            throw new AppError( `Product with ProductId ${currentProduct.productId} is not found`, 404)
+            throw new AppError(`Product with ProductId ${currentProduct.productId} is not found`, 404)
         }
 
         if (item.isAvailable === false || item.stock <= 0) {
-            throw new AppError( `Product ${item.productName || currentProduct.productId} is out of stock`, 400)
+            throw new AppError(`Product ${item.productName || currentProduct.productId} is out of stock`, 400)
         }
 
-       
+
         formattedProducts.push({
             productInfo: {
                 productId: item.productId,
                 name: item.productName,
                 altName: item.altName,
                 discription: item.description,
-                lablePrice: item.lablePrice,
-                price: item.price, 
+                lablePrice: item.labelPrice, // Send it to DB as lablePrice to match schema typo
+                price: item.price,
                 images: item.img[0],
             },
             quantity: currentQuantity
         });
 
-        total += item.lablePrice * currentQuantity;
-        lableTotal += item.lablePrice * currentQuantity;
+        total += item.labelPrice * currentQuantity;
     }
 
     const newOrder = new Order({
@@ -58,7 +57,6 @@ export async function CreateOrder(orderInfo, orderUser) {
         name: customerName,
         address: orderInfo.address,
         total: total,
-        lableTotal: lableTotal,
         phone: orderInfo.phone,
         orderProducts: formattedProducts,
     });
@@ -66,6 +64,6 @@ export async function CreateOrder(orderInfo, orderUser) {
     return await newOrder.save()
 }
 
-export  async function GetAllOrder(){
+export async function GetAllOrder() {
     return await Order.find()
 }
